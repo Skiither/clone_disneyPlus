5235529e866084a978deed7028ef726376a8c58c
Fernando Neves <fernandodn3006@gmail.com>
2025-07-07 09:07:59 +0100

    corrigindo js

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,9 @@ const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');  
 
 function scripts() {
-    return gulp.src('./src/scripts/*js.')
-    .pipe(uglify())
-    .pipe(gulp.dest('./dist/js'))
+    return gulp.src('./src/scripts/*.js') 
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'));
 }
 
 function styles() {
@@ -17,15 +17,17 @@ function styles() {
 }
 
 function images() {
-    console.log('Compilando arquivos SCSS...');
+    console.log('Otimizando imagens...');
     return gulp.src('./src/images/**/*')
         .pipe(imagemin())
         .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = gulp.parallel(styles,images,scripts);
+// build padrãoexports.default = gulp.parallel(styles, images, scripts);
+
 
 exports.watch = function () {
     gulp.watch('./src/styles/**/*.scss', gulp.parallel(styles));
-    gulp.watch('./src/scripts/*.js',gulp.parallel(scripts))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
+    gulp.watch('./src/images/**/*', gulp.parallel(images)); // adiciona watch das imagens também
 };
